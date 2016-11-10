914d2ee3e4a840cb61a4106b766f1095360f3e7e
TheLoveHandle <danielsalexandre141@yahoo.com>
2016-11-09 16:27:32 -0800

    Made the Sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,1 @@
-<div id="aside">
-    
-    <?php if(get_post_meta($post->ID, 'Quote', true));?>
-    
-    <blockquote><p><?php echo get_post_meta($post->ID, 'Quote', true);?></p></blockquote>
-    
-    <?php dynamic_sidebar(1); ?> 
-</div>
+<?php dynamic_sidebar(1); ?> 
