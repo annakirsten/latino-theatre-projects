b143bc4b9f0f1a5337bd02a09aa9a9566b393975
annakirsten <annakirsten@Annas-MacBook-Pro.local>
2016-11-15 20:48:23 -0800

    sidebar functionality

--- searchform.php
+++ searchform.php
@@ -1,14 +1,14 @@
 <div id="search">
     
     <form role="search" method="get" class="searchform group" action="<?php echo home_url( '/' ); ?>">
-         <label>
-         <div class="offscreen"><?php _x( 'Search: ', 'label' ) ?></div>
-         <input type="search" class="search-field"
-         placeholder="<?php echo esc_attr_x( ' Search', 'placeholder' ) ?>"
-         value="<?php echo get_search_query() ?>" name="s"
-         title="<?php esc_attr_x( 'Search: ', 'label' ) ?>" />
-         </label>
-         <input type="button" value="Search" alt="Submit search query">
+        <label>
+        <div class="offscreen"><?php _x( 'Search: ', 'label' ) ?></div>
+        <input type="search" class="search-field"
+        placeholder="<?php echo esc_attr_x( ' Search', 'placeholder' ) ?>"
+        value="<?php echo get_search_query() ?>" name="s"
+        title="<?php esc_attr_x( 'Search: ', 'label' ) ?>" />
+        </label>
+        <input type="submit" class="search-submit" value="SEARCH" /> 
     </form>
     
 </div>
--- sidebar.php
+++ sidebar.php
@@ -4,7 +4,17 @@
     
     <h3>Current Productions</h3>
     
+    <ul>
+        <a href="#"><li>Mauris placerat eleifend</li></a>
+    </ul>
+    
     <h3>Past Productions</h3>
+    <ul>
+        <a href="#"><li>Vestibulum erat wisi</li></a>
+        <a href="#"><li>Quisque sit amet</li></a>
+        <a href="#"><li>Aenean fermentum</li></a>
+        <a href="#"><li>Curabitur massa</li></a>
+    </ul>
     
     <h3>Support Us</h3>
     <div class="support button">
