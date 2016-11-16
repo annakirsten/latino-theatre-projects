ada09b2bb2cfc69f82f8bbf8a8aa5599cb341d85
annakirsten <annakirsten@Annas-MacBook-Pro.local>
2016-11-15 19:18:58 -0800

    added styling

--- searchform.php
+++ searchform.php
@@ -2,14 +2,13 @@
     
     <form role="search" method="get" class="searchform group" action="<?php echo home_url( '/' ); ?>">
          <label>
-         <div class="offscreen"><?php echo _x( '<h3>Search: </h3>', 'label' ) ?></div>
+         <div class="offscreen"><?php _x( 'Search: ', 'label' ) ?></div>
          <input type="search" class="search-field"
          placeholder="<?php echo esc_attr_x( ' Search', 'placeholder' ) ?>"
          value="<?php echo get_search_query() ?>" name="s"
-         title="<?php echo esc_attr_x( '<h3>Search: </h3>', 'label' ) ?>" />
+         title="<?php esc_attr_x( 'Search: ', 'label' ) ?>" />
          </label>
-         <input type="image" alt="Submit search query"
-         src="<?php echo get_template_directory_uri(); ?>/images/search-icon.png">
+         <input type="button" value="Search" alt="Submit search query">
     </form>
     
 </div>
