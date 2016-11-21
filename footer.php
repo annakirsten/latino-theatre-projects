3a1e2ed8e7b97c89ad02843bbeddb8992692392c
Adam (from home) <adamrwoodworth@gmail.com>
2016-11-21 00:53:29 -0800

    Applied styles and structure to footer
    Footer now uses columns (with responsive styles in media queries) to
    arrange footer-nav items. a class of "br" is applied to all but the last
    item in each group to prevent them from breaking up across columns.
    
    Note: Still need to add search and blog. Will likely need sone help with
    the blog bit, but I'll give it a few more tries first.

--- footer.php
+++ footer.php
@@ -1,53 +1,36 @@
+
 <div id="footer">
-
-    <div id="footer-nav">
-        
-        <ul>
-            <li><a href="<?php bloginfo('url'); ?>/contact">Contact</a></li>
-            <ul>
-                <li>eMail Fernando at:<br>fernando@email.com</li>
-                <li>eMail Someone Else at<br>someoneelse@email.com</li>
-            </ul>
-            <li><a href="<?php bloginfo('url'); ?>/support">Support Us</a></li>
-            <ul>
-                <li><a href="<?php bloginfo('url'); ?>/volunteer">Volunteer</a></li>
-                <li><a href="<?php bloginfo('url'); ?>/donate">Donate</a></li>
-            </ul>
-            <li><a href="<?php bloginfo('url'); ?>/blog">Blog</a></li>
-            <div class="footer-blog">
-
-                <li><a href="<?php bloginfo('url'); ?>/productions">Productions</a></li>
-                <ul>
-                    <li><a href="<?php bloginfo('url'); ?>/currprod">Current Productions</a></li>
-                    <li><a href="<?php bloginfo('url'); ?>/pastprod">Past  Productions</a></li>
-                </ul>
-                <li><a href="<?php bloginfo('url'); ?>/about">About us</a></li>
-                <ul>
-                    <li><a href="<?php bloginfo('url'); ?>/whatis">What Is LTP?</a></li>
-                    <li><a href="<?php bloginfo('url'); ?>/staff">Staff</a></li>
-                    <li><a href="<?php bloginfo('url'); ?>/donors">Donors and Sponsors</a></li>
-                </ul>
-                <li>&copy; Latino Theater Projects</li>
-                <ul>
-                    <li><a href="<?php bloginfo('url'); ?>/disclaimer">Disclaimer</a></li>
-                    <li><a href="<?php bloginfo('url'); ?>/policy">Privacy Policy</a></li>
-                    <li><a href="<?php bloginfo('url'); ?>/mission">Mission Statement</a></li>
-                </ul>
-            </div>
-        </ul>
-        
-    </div> <!-- end footer-nav -->
-    
-    <div id= "footer-copy">
-        <ul>
-            <li>&copy;<?php echo date("Y"); ?> Latino Theater Projects</li>
-            <li><a href="<?php bloginfo('url'); ?>/#">Privacy Policy</a></li>
-            <li><a href="<?php bloginfo('url'); ?>/#">Legal</a></li>
-            <li><?php wp_loginout(); ?></li>
-        </ul>
-    </div> <!-- end footer-copy -->
-    
-</div> <!-- end footer -->
+	<div id="footer-nav">
+		<h4 class="nb">&copy;<?php echo date("Y"); ?> Latino Theater Projects</h4>
+		<ul>
+			<li class="nb"><?php wp_loginout(); ?></li>
+			<li class="nb"><a href="<?php bloginfo('url'); ?>/disclaimer">Disclaimer</a></li>
+			<li class="nb"><a href="<?php bloginfo('url'); ?>/policy">Privacy Policy</a></li>
+			<li><a href="<?php bloginfo('url'); ?>/mission">Mission Statement</a></li>
+		</ul>
+		<h4 class="nb"><a href="<?php bloginfo('url'); ?>/about">About Us</a></h4>
+		<ul>
+			<li class="nb"><a href="<?php bloginfo('url'); ?>/whatis">What Is LTP?</a></li>
+			<li class="nb"><a href="<?php bloginfo('url'); ?>/staff">Staff</a></li>
+			<li><a href="<?php bloginfo('url'); ?>/donors">Donors and Sponsors</a></li>
+		</ul>
+		<h4 class="nb"><a href="<?php bloginfo('url'); ?>/productions">Productions</a></h4>
+		<ul>
+			<li class="nb"><a href="<?php bloginfo('url'); ?>/currprod">Current Productions</a></li>
+			<li><a href="<?php bloginfo('url'); ?>/pastprod">Past  Productions</a></li>
+		</ul>
+		<h4 class="nb"><a href="<?php bloginfo('url'); ?>/support">Support Us</a></h4>
+		<ul>
+			<li class="nb"><a href="<?php bloginfo('url'); ?>/volunteer">Volunteer</a></li>
+			<li><a href="<?php bloginfo('url'); ?>/donate">Donate</a></li>
+		</ul>
+		<h4 class="nb"><a href="<?php bloginfo('url'); ?>/contact">Contact</a></h4>
+		<ul>
+			<li class="nb">eMail Fernando at:<br>fernando@email.com</li>
+			<li>eMail Someone Else at:<br>someoneelse@email.com</li>
+		</ul>
+	</div> <!-- End footer-nav -->
+</div> <!-- End footer -->
     
 <?php wp_footer(); ?>
     
