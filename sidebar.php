839d7692930c72673361d3476c708ea5d7494dff
annakirsten <annakirsten@Annas-MacBook-Pro.local>
2016-11-23 19:51:32 -0800

    conflict fixes

--- sidebar.php
+++ sidebar.php
@@ -3,15 +3,15 @@
     <?php get_search_form(); // custom search field ?>
     
     <h3>Current Production</h3>
-        <!-- Begin Current Productions Loop -->
-        <?php rewind_posts(); // stop loop one ?>
-        <?php query_posts('post_type=event'); // show productions ?>
-        <?php if (have_posts()) : ?><!-- Begin Current Productions if -->
-        <?php while (have_posts()) : the_post(); ?><!-- Begin Current Productions while -->
-        <?php
+    <!-- Begin Current Productions Loop -->
+    <?php rewind_posts(); // stop loop one ?>
+    <?php query_posts('post_type=event'); // show productions ?>
+    <?php if (have_posts()) : ?><!-- Begin Current Productions if -->
+    <?php while (have_posts()) : the_post(); ?><!-- Begin Current Productions while -->
+    <?php
         global $post;
         global $wp_locale;        
-     
+
         // Gets the event start month from the meta field
         $start_month = get_post_meta( $post->ID, '_start_month', true );
         // Converts the month number to the month name
@@ -29,27 +29,27 @@
         $end_day = get_post_meta( $post->ID, '_end_day', true );
         // Gets the event start year
         $end_year = get_post_meta( $post->ID, '_end_year', true );
-        
+
         //gets today's date for comparison
         $today_date = current_time('Ymd');
         $compare_date = $end_year . $end_month . $end_day;
-        ?>
-        
-        <?php if($today_date<$compare_date) :?>
-            <ul>
-                <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-                
-                <h3><?php echo $start_month_word . ' ' . $start_day . ' ' . $start_year; ?> to <?php echo $end_month_word . ' ' . $end_day . ' ' . $end_year; ?> </h3>
+    ?>
 
-                <a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
+    <?php if($today_date<$compare_date) :?>
+        <ul>
+            <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 
-                <?php the_excerpt(); ?>
-                <p class="full-story"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>">Full Story &raquo;</a></p>
-            </ul>
-        <?php endif; ?><!-- End compare if --> 
-        <?php endwhile; ?><!-- End Current Productions while -->
-        <?php endif; ?> <!-- End Current Productions if -->
-        <!-- End Current Productions Loop -->
+            <h3><?php echo $start_month_word . ' ' . $start_day . ' ' . $start_year; ?> to <?php echo $end_month_word . ' ' . $end_day . ' ' . $end_year; ?> </h3>
+
+            <a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
+
+            <?php the_excerpt(); ?>
+            <p class="full-story"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>">Full Story &raquo;</a></p>
+        </ul>
+    <?php endif; ?><!-- End compare if --> 
+    <?php endwhile; ?><!-- End Current Productions while -->
+    <?php endif; ?> <!-- End Current Productions if -->
+    <!-- End Current Productions Loop -->
     
     <h3>Past Productions</h3>
     <!-- Begin Past Productions Loop -->
@@ -58,7 +58,7 @@
     <?php if (have_posts()) : ?><!-- Begin Past Productions if -->
     <?php while (have_posts()) : the_post(); ?><!-- Begin Past Productions while -->
     <?php
-    global $post;
+        global $post;
         global $wp_locale;        
      
         // Gets the event start month from the meta field
