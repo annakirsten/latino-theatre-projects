2dd756239f81c8c1f2cfd5b13bcdd4263583985a
Adam Woodworth <adamrwoodworth@gmail.com>
2016-11-23 22:22:21 -0800

    Footer Changes from Tuesday

--- footer.php
+++ footer.php
@@ -1,30 +1,30 @@
 
 <div id="footer">
 	<div id="footer-nav">
-		<h4 class="nb">&copy;<?php echo date("Y"); ?> Latino Theater Projects</h4>
+		<h4>&copy;<?php echo date("Y"); ?> Latino Theater Projects</h4>
 		<ul>
 			<li class="nb"><?php wp_loginout(); ?></li>
 			<li class="nb"><a href="<?php bloginfo('url'); ?>/disclaimer">Disclaimer</a></li>
 			<li class="nb"><a href="<?php bloginfo('url'); ?>/policy">Privacy Policy</a></li>
 			<li><a href="<?php bloginfo('url'); ?>/mission">Mission Statement</a></li>
 		</ul>
-		<h4 class="nb"><a href="<?php bloginfo('url'); ?>/about">About Us</a></h4>
+		<h4><a href="<?php bloginfo('url'); ?>/about">About Us</a></h4>
 		<ul>
 			<li class="nb"><a href="<?php bloginfo('url'); ?>/whatis">What Is LTP?</a></li>
 			<li class="nb"><a href="<?php bloginfo('url'); ?>/staff">Staff</a></li>
 			<li><a href="<?php bloginfo('url'); ?>/donors">Donors and Sponsors</a></li>
 		</ul>
-		<h4 class="nb"><a href="<?php bloginfo('url'); ?>/productions">Productions</a></h4>
+		<h4><a href="<?php bloginfo('url'); ?>/productions">Productions</a></h4>
 		<ul>
 			<li class="nb"><a href="<?php bloginfo('url'); ?>/currprod">Current Productions</a></li>
 			<li><a href="<?php bloginfo('url'); ?>/pastprod">Past  Productions</a></li>
 		</ul>
-		<h4 class="nb"><a href="<?php bloginfo('url'); ?>/support">Support Us</a></h4>
+		<h4><a href="<?php bloginfo('url'); ?>/support">Support Us</a></h4>
 		<ul>
 			<li class="nb"><a href="<?php bloginfo('url'); ?>/volunteer">Volunteer</a></li>
 			<li><a href="<?php bloginfo('url'); ?>/donate">Donate</a></li>
 		</ul>
-		<h4 class="nb"><a href="<?php bloginfo('url'); ?>/contact">Contact</a></h4>
+		<h4><a href="<?php bloginfo('url'); ?>/contact">Contact</a></h4>
 		<ul>
 			<li class="nb">eMail Fernando at:<br>fernando@email.com</li>
 			<li>eMail Someone Else at:<br>someoneelse@email.com</li>
