9c269fa121e9b2a59f3de426796dc57691b0de94
Adam Woodworth <adamrwoodworth@gmail.com>
2016-11-29 20:44:18 -0800

    Footer links update

--- footer.php
+++ footer.php
@@ -5,35 +5,28 @@
 			<h4>&copy;<?php echo date("Y"); ?> Latino Theater Projects</h4>
 			<ul>
 				<li><?php wp_loginout(); ?></li>
-				<li><a href="<?php bloginfo('url'); ?>/disclaimer">Disclaimer</a></li>
-				<li><a href="<?php bloginfo('url'); ?>/policy">Privacy Policy</a></li>
-				<li><a href="<?php bloginfo('url'); ?>/mission">Mission Statement</a></li>
+				<li><a href="<?php bloginfo('url'); ?>/#">Disclaimer</a></li>
 			</ul>
 		</div> <!-- end footer-sub -->
 		<div class="footer-sub">
 			<h4><a href="<?php bloginfo('url'); ?>/about">About Us</a></h4>
 			<ul>
-				<li><a href="<?php bloginfo('url'); ?>/whatis">What Is LTP?</a></li>
-				<li><a href="<?php bloginfo('url'); ?>/staff">Staff</a></li>
-				<li><a href="<?php bloginfo('url'); ?>/donors">Donors and Sponsors</a></li>
+				<li><a href="<?php bloginfo('url'); ?>/about/staff">Staff</a></li>
+				<li><a href="<?php bloginfo('url'); ?>/about/our-donors-and-sponsors">Donors and Sponsors</a></li>
 			</ul>			
 		</div> <!-- end footer-sub -->
 		<div class="footer-sub">
-			<h4><a href="<?php bloginfo('url'); ?>/productions">Productions</a></h4>
+			<h4><a href="<?php bloginfo('url'); ?>/#">Productions</a></h4>
+		</div> <!-- end footer-sub -->
+		<div class="footer-sub">
+			<h4><a href="<?php bloginfo('url'); ?>/#">Support Us</a></h4>
 			<ul>
-				<li><a href="<?php bloginfo('url'); ?>/currprod">Current Productions</a></li>
-				<li><a href="<?php bloginfo('url'); ?>/pastprod">Past  Productions</a></li>
+				<li><a href="<?php bloginfo('url'); ?>/#">Volunteer</a></li>
+				<li><a href="<?php bloginfo('url'); ?>/#">Donate</a></li>
 			</ul>
 		</div> <!-- end footer-sub -->
 		<div class="footer-sub">
-			<h4><a href="<?php bloginfo('url'); ?>/support">Support Us</a></h4>
-			<ul>
-				<li><a href="<?php bloginfo('url'); ?>/volunteer">Volunteer</a></li>
-				<li><a href="<?php bloginfo('url'); ?>/donate">Donate</a></li>
-			</ul>
-		</div> <!-- end footer-sub -->
-		<div class="footer-sub">
-			<h4><a href="<?php bloginfo('url'); ?>/contact">Contact</a></h4>
+			<h4><a href="<?php bloginfo('url'); ?>/#">Contact</a></h4>
 			<ul>
 				<li>eMail Fernando at:<br>fernando@email.com</li>
 				<li>eMail Someone Else at:<br>someoneelse@email.com</li>
