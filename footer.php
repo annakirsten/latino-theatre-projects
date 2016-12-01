c2b6f041f8b8b58230ffd19bf0d16a01d0b3021a
Adam (from home) <adamrwoodworth@gmail.com>
2016-12-01 13:28:06 -0800

    Fixed the footer
    Again
    :)

--- footer.php
+++ footer.php
@@ -2,39 +2,34 @@
 <div id="footer">
 	<div id="footer-nav">
 
-		<div class="footer-sub">
-			<h4>&copy;<?php echo date("Y"); ?> Latino Theater Projects</h4>
-			<ul>
-				<li><?php wp_loginout(); ?></li>
-				<li><a href="<?php bloginfo('url'); ?>/#">Disclaimer</a></li>
-			</ul>
-		</div> <!-- end footer-sub -->
-		<div class="footer-sub">
-			<h4><a href="<?php bloginfo('url'); ?>/about">About Us</a></h4>
-			<ul>
-				<li><a href="<?php bloginfo('url'); ?>/about/staff">Staff</a></li>
-				<li><a href="<?php bloginfo('url'); ?>/about/our-donors-and-sponsors">Donors and Sponsors</a></li>
-			</ul>			
-		</div> <!-- end footer-sub -->
-		<div class="footer-sub">
-			<h4><a href="<?php bloginfo('url'); ?>/#">Productions</a></h4>
-		</div> <!-- end footer-sub -->
-		<div class="footer-sub">
-			<h4><a href="<?php bloginfo('url'); ?>/#">Support Us</a></h4>
-			<ul>
-				<li><a href="<?php bloginfo('url'); ?>/#">Volunteer</a></li>
-				<li><a href="<?php bloginfo('url'); ?>/#">Donate</a></li>
-			</ul>
-		</div> <!-- end footer-sub -->
-		<div class="footer-sub">
-			<h4><a href="<?php bloginfo('url'); ?>/#">Contact</a></h4>
-			<ul>
-				<li>eMail Fernando at:<br>fernando@email.com</li>
-				<li>eMail Someone Else at:<br>someoneelse@email.com</li>
-			</ul>
-		</div> <!-- end footer-sub -->
-
+		<div class="footer-subnav">
+			<h3>Latino Theater Projects</h3>
+			<p><?php wp_loginout(); ?></p>
+			<p><a href="<?php bloginfo('url'); ?>/#">Disclaimer</a></p>
+		</div> <!-- end footer-subnav -->
+		<div class="footer-subnav">
+			<h3><a href="<?php bloginfo('url'); ?>/about">About Us</a></h3>
+			<p><a href="<?php bloginfo('url'); ?>/about/staff">Staff</a></p>
+			<p><a href="<?php bloginfo('url'); ?>/about/our-donors-and-sponsors">Donors and Sponsors</a></p>
+		</div> <!-- end footer-subnav -->
+		<div class="footer-subnav">
+			<h3><a href="<?php bloginfo('url'); ?>/#">Productions</a></h3>
+		</div> <!-- end footer-subnav -->
+		<div class="footer-subnav">
+			<h3><a href="<?php bloginfo('url'); ?>/#">Support Us</a></h3>
+			<p><a href="<?php bloginfo('url'); ?>/#">Volunteer</a></p>
+			<p><a href="<?php bloginfo('url'); ?>/#">Donate</a></p>
+		</div> <!-- end footer-subnav -->
+		<div class="footer-subnav">
+			<h3><a href="<?php bloginfo('url'); ?>/#">Contact</a></h3>
+			<p>Email: <a href="mailto:info@latinotheatreprojects.org">info@latinotheatreprojects.org</a><br>
+			Phone: <tel>(206)718-5077</tel><br>
+			Address: PO Box 24082,<br>Seattle, WA 98124</p>
+		</div> <!-- end footer-subnav -->
 	</div> <!-- End footer-nav -->
+	<div id="footer-copyright">
+		<p>&copy;<?php echo date("Y"); ?> Latino Theater Projects</p>
+	</div>
 </div> <!-- End footer -->
     
 <?php wp_footer(); ?>
