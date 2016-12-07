1a304db0d0e0e722a952deaaeb07f158ac284bd4
JenBailey <jb.jenbailey@gmail.com>
2016-12-06 19:21:44 -0800

    added links to the footer

--- footer.php
+++ footer.php
@@ -3,9 +3,9 @@
 	<div id="footer-nav">
 
 		<div class="footer-subnav">
-			<h3>Latino Theater Projects</h3>
+			<h3><a href="<?php bloginfo('url'); ?>/index">Latino Theater Projects</a></h3>
 			<p><?php wp_loginout(); ?></p>
-			<p><a href="<?php bloginfo('url'); ?>/#">Disclaimer</a></p>
+			<p><a href="<?php bloginfo('url'); ?>/disclaimer">Disclaimer</a></p>
 		</div> <!-- end footer-subnav -->
 		<div class="footer-subnav">
 			<h3><a href="<?php bloginfo('url'); ?>/about">About Us</a></h3>
@@ -13,15 +13,15 @@
 			<p><a href="<?php bloginfo('url'); ?>/about/our-donors-and-sponsors">Donors and Sponsors</a></p>
 		</div> <!-- end footer-subnav -->
 		<div class="footer-subnav">
-			<h3><a href="<?php bloginfo('url'); ?>/#">Productions</a></h3>
+			<h3><a href="<?php bloginfo('url'); ?>/productions">Productions</a></h3>
 		</div> <!-- end footer-subnav -->
 		<div class="footer-subnav">
-			<h3><a href="<?php bloginfo('url'); ?>/#">Support Us</a></h3>
-			<p><a href="<?php bloginfo('url'); ?>/#">Volunteer</a></p>
-			<p><a href="<?php bloginfo('url'); ?>/#">Donate</a></p>
+			<h3><a href="<?php bloginfo('url'); ?>/support-us">Support Us</a></h3>
+			<p><a href="<?php bloginfo('url'); ?>/donate">Donate</a></p>            
+			<p><a href="<?php bloginfo('url'); ?>/volunteer">Volunteer</a></p>
 		</div> <!-- end footer-subnav -->
 		<div class="footer-subnav">
-			<h3><a href="<?php bloginfo('url'); ?>/#">Contact</a></h3>
+			<h3><a href="<?php bloginfo('url'); ?>/contact">Contact</a></h3>
 			<p>Email: <a href="mailto:info@latinotheatreprojects.org">info@latinotheatreprojects.org</a><br>
 			Phone: <tel>(206)718-5077</tel><br>
 			Address: PO Box 24082,<br>Seattle, WA 98124</p>
