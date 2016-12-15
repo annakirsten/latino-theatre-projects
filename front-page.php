a5b42d67b22cfd2353de7fd4ecdd57624395485f
TheLoveHandle <danielsalexandre141@yahoo.com>
2016-12-14 21:36:34 -0800

    no more recent posts for blog

--- front-page.php
+++ front-page.php
@@ -17,24 +17,6 @@
             <p><a href="<?php bloginfo('url'); ?>/#">Donate</a></p>
         </div>
 
-        <div id="recent-posts">
-            <h2>Recent Blog Post</h2>
-            <?php rewind_posts(); //stop loop one ?>
-            <?php query_posts('showposts=1'); //give loop two directions ?>
-            <?php if (have_posts()) : while(have_posts()) : the_post(); //start the loop ?>
-                <article id="<?php the_ID(); ?>" class="blog-excerpt">
-                    <h3><a href="<?php the_permalink(); //link to the page/posting ?>"><?php the_title(); //get the title?></a></h3>
-                    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); //display the post featured image as thumbnail size ?></a>
-                    <small><?php the_time('F j, Y'); ?></small><br />
-                    <?php the_excerpt(); //show the excerpt ?>
-                    <p class="read-more"><a href="<?php the_permalink() ?>">Read More&nbsp;&raquo;</a></p>
-                </article>
-            <?php endwhile; endif; //end the loop ?>
-            
-            <a href="<?php bloginfo('url'); ?>/blog">Go to Blog&nbsp;&raquo;</a>
-            
-        </div>
-
         <!-- <p>front-page.php</p> -->
 
     </div>
